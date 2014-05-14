1f265b1a746d3af964eb11506d6f7a48e0d0e828
Luke Wang <luke.wang1990@gmail.com>
2014-05-15 02:40:31 +0800

    update game-ui.js and login.js

--- app/js/game-ui.js
+++ app/js/game-ui.js
@@ -79,6 +79,13 @@ $(document).ready(function(event) {
 		}
 	});
 	
+	// prepare data to send to the game server
+	data = new Object();
+	data.readyList = localStorage(getItem('readyList'));
+	data.num = num;
+	data.score = 5;
+	data.shape = shape;
+	gameConn.emit('enter config', data);
 
 	scoreConn.on('score-update', function () {
 		console.log(data);
@@ -117,16 +124,9 @@ $(document).ready(function(event) {
 	$('#ready-button').click(function(event) {
 		event.preventDefault();
 		event.stopPropagation();
-		// prepare data to send
-		data = new Object();
-		data.index = index;
-		data.num = num;
-		data.name = name;
-		data.score = 5;
-		data.shape = shape;
 		// send data
 		lobbyConn.emit('game ready', playerID);
-		/// TODO: emit to game route
+		// gameConn.emit('game ready', playerID);
 	});
 
 	$('#return-button').click(function(event) {
